a22b154cd45e72110466bbe25612824c96b918a5
Ben Collins <ben.c@servergy.com>
2015-03-01 20:19:30 -0500

    SVY: Integrate SlackAPI.php
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- lib/hooks.php
+++ lib/hooks.php
@@ -1,14 +1,11 @@
 <?php
 				
 class OC_SlackNotify_Hooks{
-	
-	
 	/**
 	 *  Notify a new file/forder creation or change.
 	 */
-	static public function notify($path) {
-		OC_MailNotify_Mailing::queue_fileChange_notification($path);
-		
+	static public function notify_create($path) {
+		OC_SlackNotify::slack_notification_create($path);
 		return true;
 	}
 }
