5217fe0838470114129632ca760c4166ae3ba332
Ben Collins <ben.c@servergy.com>
2015-03-03 23:59:40 -0500

    SVY: Add comment to Slack settings for configuring which alerts to receive
    Our code relies on the activity app, because we hook into an existing
    way of capturing activity.
    
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- templates/settings-personal.php
+++ templates/settings-personal.php
@@ -2,10 +2,11 @@
 	<h2><img width="18" src="<?php p(OCP\Util::imagePath('slacknotify', 'slack-sticker.png')); ?>"/>
         <?php p($l->t('Slack Integration')); ?></h2>
 	<?php if ($_['slackEnabled']) { ?>
-		<p><?php p($l->t('You have integrated Slack.')); ?></p>
+		<p><?php p($l->t('In order to limit which notifications you receive, adjust the "Activity" settings for "Stream".')); ?></p>
+		<p><?php p($l->t('Slack integration is enabled.')); ?></p>
 		<button type="button" name="disableSlack"><?php p($l->t("Disable Slack")); ?></button>
 	<?php } else { ?>
-		<p><?php p($l->t('You can enable Slack by clicking below.')); ?></p>
+		<p><?php p($l->t('You can enable Slack integration by clicking below.')); ?></p>
 		<button type="button" name="enableSlack"><?php p($l->t("Enable Slack")); ?></button>
 		<input type="hidden" id="auth_url" name="auth_url" value="<?php p($_['slackAuthUrl']); ?>">
 	<?php } ?>
