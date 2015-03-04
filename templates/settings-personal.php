84f3d3890f9547aee5cc1076558e060cb5cf9f91
Ben Collins <ben.c@servergy.com>
2015-03-04 09:42:58 -0500

    SVY: Update note about Activity settings.
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- templates/settings-personal.php
+++ templates/settings-personal.php
@@ -2,7 +2,7 @@
 	<h2><img width="18" src="<?php p(OCP\Util::imagePath('slacknotify', 'slack-sticker.png')); ?>"/>
         <?php p($l->t('Slack Integration')); ?></h2>
 	<?php if ($_['slackEnabled']) { ?>
-		<p>In order to limit which notifications you receive, adjust the <a href="#activity_notifications">Activity</a> settings for "Stream".</p>
+		<p>In order to limit which notifications you receive, adjust the <a href="#activity_notifications">Activity</a> settings for "Mail".</p>
 		<p><?php p($l->t('Slack integration is enabled.')); ?></p>
 		<button type="button" name="disableSlack"><?php p($l->t("Disable Slack")); ?></button>
 	<?php } else { ?>
